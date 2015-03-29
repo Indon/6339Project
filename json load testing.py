59d2d452e23db52488a8c623a3f3fc36d5c58d41
Indon <indondasani@yahoo.com>
2015-03-29 15:15:23 -0500

    Setup Fiddling
    Moving some stuff around in preparation to do project work

---
+++ json load testing.py
@@ -0,0 +1,14 @@
+#Earl White
+#Data Mining Project, Test File
+#1000882451
+
+import json
+
+
+class Test_junk:
+    def __init__(self):
+        pass
+        
+#This by itself should complete the classification on train.csv and produce a classification file for test.csv.
+Stuff = Test_junk()
+
